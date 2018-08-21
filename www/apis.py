35082aea329a5016674d596b076f70a1cf322dc0
qianlinwu <Simpleran@sina.com>
2018-08-21 12:01:02 +0800

    代码风格调整为PEP8

--- www/apis.py
+++ www/apis.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'duke.wu'
+__author__ = "duke.wu"
 
-'''
+"""
 JSON API definition.
-'''
+"""
 
-import json, logging, inspect, functools
 
 class Page(object):
-    '''
+    """
     Page object for display pages.
-    '''
+    """
 
     def __init__(self, item_count, page_index=1, page_size=10):
-        '''
+        """
         Init Pagination by item_count, page_index and page_size.
 
         >>> p1 = Page(100, 1)
@@ -39,7 +38,7 @@ class Page(object):
         90
         >>> p3.limit
         10
-        '''
+        """
         self.item_count = item_count
         self.page_size = page_size
         self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
@@ -59,37 +58,42 @@ class Page(object):
 
     __repr__ = __str__
 
+
 class APIError(Exception):
-    '''
+    """
     the base APIError which contains error(required), data(optional) and message(optional).
-    '''
+    """
     def __init__(self, error, data='', message=''):
         super(APIError, self).__init__(message)
         self.error = error
         self.data = data
         self.message = message
 
+
 class APIValueError(APIError):
-    '''
+    """
     Indicate the input value has error or invalid. The data specifies the error field of input form.
-    '''
+    """
     def __init__(self, field, message=''):
         super(APIValueError, self).__init__('value:invalid', field, message)
 
+
 class APIResourceNotFoundError(APIError):
-    '''
+    """
     Indicate the resource was not found. The data specifies the resource name.
-    '''
+    """
     def __init__(self, field, message=''):
         super(APIResourceNotFoundError, self).__init__('value:notfound', field, message)
 
+
 class APIPermissionError(APIError):
-    '''
+    """
     Indicate the api has no permission.
-    '''
+    """
     def __init__(self, message=''):
         super(APIPermissionError, self).__init__('permission:forbidden', 'permission', message)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import doctest
     doctest.testmod()
