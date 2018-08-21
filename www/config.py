d595519cc5488a15e23882531f35a1e439f2ab42
qianlinwu <Simpleran@sina.com>
2018-08-21 15:37:07 +0800

    PEP 8 日志格式调整。

--- www/config.py
+++ www/config.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-'''
+"""
 Configuration
-'''
+"""
 
 __author__ = 'duke.wu'
 
@@ -10,9 +10,9 @@ import config_default
 
 
 class Dict(dict):
-    '''
+    """
     Simple dict but support access as x.y style.
-    '''
+    """
     def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
         for k, v in zip(names, values):
